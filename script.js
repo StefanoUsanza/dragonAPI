805b82d995e2b12297b0c15ed5b76d34179ddcd9
StefanoUsanza <stefano.usanza@gmail.com>
2020-11-14 18:22:06 +0100

    add search

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-function info(b){
-  const name = document.getElementById("name")
-  const name1 = document.createElement("h1")
+function search(b){
+  const mon= document.getElementById("monsters")
+  let mon1= document.createElement("option")
 
-  const info = document.getElementById("info")
-  const info3 = document.createElement("h3")
+  for(let i=0; i<322; i++){
+    mon1.innerHTML =b.results[i].index
+    mon.appendChild(mon1)
+    mon1= document.createElement("option")
+  }
 
+}
+//creo gli elementi fuori dalla funzione per sovrascriverli e non crearne di nuovi ad ogni chiamata
+const name = document.getElementById("name")
+const name1 = document.createElement("h1")
+const info = document.getElementById("info")
+const info3 = document.createElement("h3")
+
+function infos(b){
   name1.innerHTML=b.name
   name.appendChild(name1)
 
@@ -12,8 +23,7 @@ function info(b){
   info.appendChild(info3)
 }
 
-function stats(b){
-
+//creo gli elementi fuori dalla funzione per sovrascriverli e non crearne di nuovi ad ogni chiamata
   const str= document.getElementById("str")
   const str2= document.createElement("h2")
 
@@ -32,6 +42,7 @@ function stats(b){
   const cha= document.getElementById("cha")
   const cha2= document.createElement("h2")
 
+function stats(b){
   str2.innerHTML="<h2 style='text-align: center; color: white;'>" + b.strength + "</h2>"
   str.appendChild(str2)
 
@@ -52,8 +63,7 @@ function stats(b){
 
 }
 
-function stat(b){
-
+//creo gli elementi fuori dalla funzione per sovrascriverli e non crearne di nuovi ad ogni chiamata
 const armor_class = document.getElementById("armor_class")
 const armor1 = document.createElement("h1")
 
@@ -62,6 +72,10 @@ const point1 = document.createElement("h1")
 
 const speed = document.getElementById("speed")
 const speed1 = document.createElement("h1")
+
+function stat(b){
+
+
   //il punto serve per avere le immagini alla stessa altezza, lo rendo dello stesso colore dello sfondo
   armor1.innerHTML=b.armor_class + "<br><p style='color: DarkSlateGray;'>.</p>"
   armor_class.appendChild(armor1)
@@ -73,10 +87,20 @@ const speed1 = document.createElement("h1")
   speed.appendChild(speed1)
 }
 
-fetch("https://www.dnd5eapi.co/api/monsters/adult-blue-dragon").then(r => r.json())
-.then(b => {
-  console.log(b.strength)
-  info(b)//nome e informazioni del mostro
-  stat(b)//classe armatura vita velocità
-  stats(b)//statistiche
+const btnSearch = document.getElementById("search")
+
+fetch("https://www.dnd5eapi.co/api/monsters").then(r => r.json())
+.then(b => { 
+  search(b)//inserisci elementi nella barra di ricerca
+  }).then(() =>{
+    btnSearch.onclick  = () => {
+      const input = document.getElementById("input").value
+      fetch("https://www.dnd5eapi.co/api/monsters/" + input).then(r => r.json())
+      .then(b => {
+        infos(b)//nome e informazioni del mostro
+        stat(b)//classe armatura vita velocità
+        stats(b)//statistiche
+      })
+    }
   })
+ 
